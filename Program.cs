4959f91adb5ae36322f380a1f8679543a42bb39f
Jorge <jepers2021@gmail.com>
2021-03-27 18:35:57 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A133590.Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 15.");
+            Console.WriteLine("Programa que determina si un ingreso es un valor numérico.");
+            Console.Write("Por favor, ingrese un número: ");
+            double ingreso;
+            bool exito = Double.TryParse(Console.ReadLine(), out ingreso);
+            if(exito)
+            {
+                Console.WriteLine("El ingreso puede ser interpretado como un valor numérico.");
+            }
+            else
+            {
+                Console.WriteLine("El ingreso no puede ser interpretado como un valor numérico.");
+            }
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+
+        }
+    }
+}
